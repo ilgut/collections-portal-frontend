4c01e61929cb182fe6ddff22ef8c2f9befb88d7b
Zfex <55106553+Zfex@users.noreply.github.com>
2025-06-27 16:07:52 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { AuthProvider } from "@/auth/AuthContext";
 import Navbar from '@/pages/Navbar';
 import Edit from "./pages/Edit";
 import Walory from "./pages/Walory";
+import WalorInfo from "./pages/WalorInfo";
+import Friends from "./pages/Friends";
 
 function App() {
     return (
@@ -18,7 +20,9 @@ function App() {
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/edit" element={<Edit />} />
+                    <Route path="/friends" element={<Friends />} />
                     <Route path="/walory" element={<Walory />} />
+                    <Route path="/walor/:id" element={<WalorInfo />} />
                 </Routes>
             </Router>
         </AuthProvider>
